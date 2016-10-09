c9256cb7f74377a2121b69e4260710ab5033f1fd
Mandolin blog <mv04301902@gmail.com>
2016-10-09 03:53:00 +0000

    create and destroy (logout) pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'login', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
