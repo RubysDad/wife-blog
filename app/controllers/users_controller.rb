66b55f2bee4f4066b94238fe383dc281be1bfc3b
Mandolin blog <mv04301902@gmail.com>
2016-10-21 23:08:39 +0000

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Hephzibah #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -58,11 +59,11 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
-  
+
   def require_admin
     if logged_in? and !current_user.admin?
       flash[:danger] = "Only admin users can perform that action"
       redirect_to root_path
     end
-  end 
+  end
 end
